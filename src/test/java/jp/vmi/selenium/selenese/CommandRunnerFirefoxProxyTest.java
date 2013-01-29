34733f5e6fe01ff9ad053c01b792bdec90e61414
Hayato ITO <haya10.ito@gmail.com>
2013-01-30 00:45:42 +0900

    proxy resource rule to class rule for no web access testcases.

--- src/test/java/jp/vmi/selenium/selenese/CommandRunnerFirefoxProxyTest.java
+++ src/test/java/jp/vmi/selenium/selenese/CommandRunnerFirefoxProxyTest.java
@@ -1,11 +1,7 @@
 package jp.vmi.selenium.selenese;
 
-import org.junit.Assume;
-import org.junit.Before;
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.rules.Verifier;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.firefox.FirefoxBinary;
 
 import com.thoughtworks.selenium.SeleniumException;
 
@@ -23,14 +19,14 @@ public class CommandRunnerFirefoxProxyTest extends CommandRunnerFirefoxTest {
     /**
      * proxy resource
      */
-    @Rule
-    public WebProxyResource proxy = new WebProxyResource();
+    @ClassRule
+    public static WebProxyResource proxy = new WebProxyResource();
 
     /**
      * verify used proxy in testmethod.
      */
-    @Rule
-    public Verifier proxyused = new Verifier() {
+    @ClassRule
+    public static Verifier proxyused = new Verifier() {
         @Override
         protected void verify() throws Throwable {
             assertThat(proxy.getProxy().getCount(), is(greaterThan(0)));
